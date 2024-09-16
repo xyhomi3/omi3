25dd0874b64f7adb0b48019d4051aa884602ae8d
omi3 <lucien.loua@gmail.com>
2024-09-16 11:34:48 +0000

    feat(routing): implement default locale redirection

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,16 +1,6 @@
-import { AudioPlayer } from './_components/audio';
-import { Metadata } from 'next';
+import { routing } from '@/lang';
+import { redirect } from 'next/navigation';
 
-export const metadata: Metadata = {
-  title: 'Player',
-};
-
-export default function Home() {
-  return (
-    <main className="flex flex-grow items-center justify-center p-5" role="main">
-      <div className="w-full max-w-md">
-        <AudioPlayer />
-      </div>
-    </main>
-  );
+export default function RootPage() {
+  redirect(`/${routing.defaultLocale}`);
 }
